8477786021b162f1903fe5cd7fd4155a17fd3743
rentonhead <hasancemilacar@gmail.com>
2023-06-04 04:40:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import City from "./City"
 
 function App() {
+  const key = "2a8c5baffef49d8bdb18b6395f399f6c";
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState();
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`
+        );
+        console.log(response);
+        setCity(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  }, [search]);
+  console.log(search);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+          <input
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Placeholder"
+            className="px-3 py-3 w-[250px] placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring"
+          />
+          <City city={city} />
+      </div>
     </div>
   );
 }
